1ab79f8e417583c794b6dde33721deb893bbb4b0
sourcesemsem <122203365+sourcesemsem@users.noreply.github.com>
2023-01-08 12:26:26 +0200

    Update Source.py

--- SQL/extras/Source.py
+++ SQL/extras/Source.py
@@ -5,7 +5,7 @@ from Arab import StartTime, iqthon
 from Arab.Config import Config
 from Arab.plugins import mention
 help1 = ("**🝳 ⦙ كيفيه التنصيب :**")
-help2 = ("**🝳 ⦙ قـائمـه الاوامـر :**\n**🝳 ⦙ قنـاه السـورس :** @pp_g3  ")
+help2 = ("**🝳 ⦙ قـائمـه الاوامـر :**\n**🝳 ⦙ قنـاه السـورس :** @FTTUTY  ")
 TG_BOT = Config.TG_BOT_USERNAME
 TM = time.strftime("%I:%M")
-Sour = f"**‎⿻┊My 𖠄 {mention} ٫ **\n‌‎**⿻┊BoT 𖠄 {TG_BOT} ٫**\n**‌‎⿻┊TimE 𖠄 {TM} ٫**\n**‌‎⿻┊‌‎VeRsIoN 𖠄 (7.7) ,** \n**⿻┊‌‎Mrs. zein 𖠄** @iiqllll"
+Sour = f"**‎⿻┊My 𖠄 {mention} ٫ **\n‌‎**⿻┊BoT 𖠄 {TG_BOT} ٫**\n**‌‎⿻┊TimE 𖠄 {TM} ٫**\n**‌‎⿻┊‌‎VeRsIoN 𖠄 (7.7) ,** \n**⿻┊‌‎˹ 𝘴 ꪖ ꪑ 𝓲 𝘳 ˼  𖠄** @DEV_SAMIR"
