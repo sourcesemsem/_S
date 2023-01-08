3f3db27c83e7e9d860d794a421108c648be0b52e
sourcesemsem <122203365+sourcesemsem@users.noreply.github.com>
2023-01-08 15:28:08 +0200

    Update Source.py

--- SQL/extras/Source.py
+++ SQL/extras/Source.py
@@ -8,4 +8,4 @@ help1 = ("**🝳 ⦙ كيفيه التنصيب :**")
 help2 = ("**🝳 ⦙ قـائمـه الاوامـر :**\n**🝳 ⦙ قنـاه السـورس :** @FTTUTY  ")
 TG_BOT = Config.TG_BOT_USERNAME
 TM = time.strftime("%I:%M")
-Sour = f"**‎⿻┊My 𖠄 {mention} ٫ **\n‌‎**⿻┊BoT 𖠄 {TG_BOT} ٫**\n**‌‎⿻┊TimE 𖠄 {TM} ٫**\n**‌‎⿻┊‌‎VeRsIoN 𖠄 (7.7) ,** \n**⿻┊‌‎˹ 𝘴 ꪖ ꪑ 𝓲 𝘳 ˼  𖠄** @DEV_SAMIR"
+Sour = f"**‎✰︎┊ᴍʏ 𖠄 {mention} ٫ **\n‌‎**✰︎┊ʙᴏᴛ 𖠄 {TG_BOT} ٫**\n**‌‎✰︎┊ᴛɪᴍᴇ 𖠄 {TM} ٫**\n**‌‎✰︎┊‌‎ᴠᴇʀsʟᴏɴ 𖠄 (7.7) ,** \n**✰︎┊‌‎ᴅᴇᴠ sᴀᴍɪʀ 𖠄** @DEV_SAMIR"
